04f23388e0116a9e08d2b7b5c361461ac87c5996
T!M Greller <timlg07@gmail.com>
2019-08-10 12:40:11 +0200

    created script with UserScript-comment

---
+++ Darksite.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Darksite
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  browse the dark side
+// @author       Tim L. Greller
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
