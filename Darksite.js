05918e7a0430e4df68067cf8fe8bf4840aea5c82
T!M Greller <timlg07@gmail.com>
2019-08-10 12:42:06 +0200

    first test
    provides monochrome colors suited for reading articles

--- Darksite.js
+++ Darksite.js
@@ -7,3 +7,10 @@
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
+
+window.addEventListener('load', function(){
+    document.querySelectorAll("*").forEach(function(_){
+        _.style.color = "#c3c0b8";
+        _.style.backgroundColor = "#1d1511";
+    });
+});
